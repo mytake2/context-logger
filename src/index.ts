5184a07b8bf05122d7c5e6bdbe31156ef59b6dfa
wesleythorsen1 <wesley.thorsen@gmail.com>
2025-07-30 13:32:20 -0400

    feat: switch default logger to winston

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export * from '@mytake2/context-logger/core';
 
-// use console implementation by default.
-// can be swapped for other implementations (winston, pino, log4js, console)
+// use winston implementation by default.
+// can be swapped for other implementations (console, pino, log4js, console)
 // but would warrant a major version bump due to options model change.
-export { createLogger, logger } from '@mytake2/context-logger/console';
+export { createLogger, logger } from '@mytake2/context-logger/winston';
 
 // // to use winston implementation:
 // export { createLogger, logger } from '@mytake2/context-logger/winston';
